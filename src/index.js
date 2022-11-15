f0586e78db6bfeea12730ff54b99d90bae3440a3
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-15 13:32:32 +0545

    message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from "./Cards";
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -7,22 +8,29 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
-  <div className='cards'>
-    <div className='card'>
-      <img src="disable.jpg" height="300px" width="300px"  alt="myPic" className='card_img'></img>
-      <div className='card_info'>
-        <span className='"card_category'>A Netflix Original Series</span>
-        <h3 className='card_title'>DARK</h3>
-        <a href="" target="blank">
-          <button>Watch Now</button>
-        </a>
-      </div>
-    </div>    
-  </div>
- </>,
- document.getElementById('root')
-);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+  <Card imgsrc="disable.jpg"
+  sname= "DARK"
+   title="A Netflix Original Series"
+   
+   link="https://www.netflix.com/np/"
+   />
+  <Card imgsrc="disab.jpg"
+  sname= "Facebook"
+   title="A Netflix Original Series"
+   
+   link="https://www.facebook.com/"/>
+  <Card imgsrc="disablee.jpg"
+   title="A Netflix Original Series"
+   sname= "Instagram"
+   link="https://www.instagram.com/"/>
+
+  </>, 
+  document.getElementById("root")
+  );
+ 
+
+
+{/* // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
 //     <App />
@@ -32,4 +40,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// reportWebVitals(); */}
